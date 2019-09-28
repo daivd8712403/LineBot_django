36c4cf37a5f395b9e57b6cf0df42bfc755a6b8c9
David Chen <david87124@gmail.com>
2019-09-28 13:21:00 +0800

    v2

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from linebot import LineBotApi, WebhookParser
+from linebot.exception import InvalidSignatureError
+from linebot.models import MessageEvent, TextSendMessage
+
+line_bot_api = LineBotApi('Channel_access_token')
+parser = WebhookParser('Channel_secret')
 
 def helloworld(request):
     return HttpResponse('Im David Hello World!')
+
+def callback(request):
+    if request.method == 'POST':
+        return HttpResponse('OK')
+    else:
+        return HttpResponse('OK')
